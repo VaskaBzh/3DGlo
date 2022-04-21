df22cecf943db24b07a8bbc6c4523421dc4170bb
Vasily <90596079+VaskaBzh@users.noreply.github.com>
2022-04-21 14:29:31 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import one from './modules/one'
-import two from './modules/two'
+import timer from './modules/timer'
+// import two from './modules/two'
 
-one() 
-two()
+timer() 
+// two()
---
+++ src/modules/timer.js
@@ -0,0 +1,48 @@
+const timer = () => {
+    const timerHours = document.getElementById('timer-hours')
+    const timerMinutes = document.getElementById('timer-minutes')
+    const timerSeconds = document.getElementById('timer-seconds')
+
+    const getTimeRemaining = (deadline) => {
+        let dateStop = new Date(deadline).getTime()
+        let dateNow = new Date().getTime()
+        let timeRemaining = (dateStop - dateNow) / 1000
+        let days = Math.floor(timeRemaining / 60 / 60 / 24)
+        let hours = Math.floor((timeRemaining / 60 / 60) % 24)
+        let minutes = Math.floor((timeRemaining / 60) % 60)
+        let seconds = Math.floor(timeRemaining % 60)
+
+        return { timeRemaining, hours, minutes, seconds }
+    }
+
+    const updateClock = () => {
+        let getTime = getTimeRemaining('24 april 2022')
+
+        if (getTime.hours > 10) {
+            timerHours.textContent = getTime.hours
+        } else {
+            timerHours.textContent = '0' + getTime.hours
+        }
+        if (getTime.minutes > 10) {
+            timerMinutes.textContent = getTime.minutes
+        } else {
+            timerMinutes.textContent = '0' + getTime.minutes
+        }
+        if (getTime.seconds > 10) {
+            timerSeconds.textContent = getTime.seconds
+        } else {
+            timerSeconds.textContent = '0' + getTime.seconds
+        }
+
+        if (getTime.timeRemaining > 0) {
+            setInterval(updateClock, 1000)
+        } else {
+            timerHours.textContent = '00'
+            timerMinutes.textContent = '00'
+            timerSeconds.textContent = '00'
+        }
+    }
+    updateClock()
+}
+
+export default timer
